39c0afe3a17a823155eb1cc896fc58796538699b
MKbenjA9 <xbenjamin629@gmail.com>
2025-06-23 15:08:24 -0400

    first commit

---
+++ src/main/java/com/example/TechFlowSpa/TechFlowSpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechFlowSpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFlowSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechFlowSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TechFlowSpa/TechFlowSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechFlowSpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFlowSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
